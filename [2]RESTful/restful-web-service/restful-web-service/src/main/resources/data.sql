c5490e97fb9497e4799c5d252bf46648a04b2493
HYEEWON <hw0916@naver.com>
2021-08-25 23:45:57 +0900

    update

--- [2]RESTful/restful-web-service/restful-web-service/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
--- 서버를 실행하면 자동으로 실행되어 DB에 값이 추가 됨.
-insert into user values(9001, sysdate(), 'User1', 'test1111', '001100-1234567');
-insert into user values(9002, sysdate(), 'User2', 'test2222', '001100-1234567');
-insert into user values(9003, sysdate(), 'User3', 'test2222', '001100-1234567');
-
-insert into post values(1001, 'My first post', 9001);
-insert into post values(1002, 'My second post', 9001);
